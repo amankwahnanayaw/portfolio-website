033eb64d6f2f479b50f5dfd23b586457a5a342f0
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-02-15 12:21:19 +0000

    working on the header

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -15,6 +15,9 @@ const Header = () => {
         <div className='app__header-badge'>
           <div className='badge-cmp app__flex'>
             <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className='p-text'>Hello, I am</p>
+            </div>
           </div>
         </div>
       </motion.div>
