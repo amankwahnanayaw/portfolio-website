f4c316d773655f5a7b3a6a639f16216fce423a3c
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-02-15 12:44:44 +0000

    working on the header

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion } from 'framer-motion';
+import { easeInOut, motion } from 'framer-motion';
 import { images } from '../../constants'
 
 import './Header.scss';
@@ -7,6 +7,7 @@ import './Header.scss';
 const Header = () => {
   return (
     <div className='app__header app__flex'>
+
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
@@ -25,6 +26,27 @@ const Header = () => {
             <p className='p-text'>Freelancer</p>
           </div>
         </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5}}
+        className='app__header-img'
+      >
+        <img src={images.profile} alt='profile_bg' />
+        <motion.div
+         whileInView={{ scale: [0, 1] }}
+         transition={{ duration: 1, ease: 'easeInOut'}}
+         src={images.circle}
+         alt='profile_circle'
+         className='overlay_circle' 
+        >
+
+        </motion.div>
+      </motion.div>
+
+      <motion.div>
+
       </motion.div>
     </div>
   )
