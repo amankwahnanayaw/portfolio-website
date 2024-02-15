a16397249a61f62908b2f0dcc753c9b1dd19ffc6
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-02-15 12:17:03 +0000

    working on the header

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -6,7 +6,19 @@ import './Header.scss';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='app__header app__flex'>
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className='app__header-info'
+      >
+        <div className='app__header-badge'>
+          <div className='badge-cmp app__flex'>
+            <span>👋</span>
+          </div>
+        </div>
+      </motion.div>
+    </div>
   )
 }
 
