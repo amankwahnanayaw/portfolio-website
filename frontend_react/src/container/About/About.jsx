332fd40975880e5692f1dc0e18d786070b03f2b6
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-02-18 18:49:30 +0000

    working the about section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 
 import './About.scss';
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <h2 className='head-text'>
+        I know That
+        <span>Good Design</span>
+        means
+        <span>Good Business</span>
+      </h2>
+    </>
   )
 }
 
