adb5091ec41ceb436aab709b662dbae7ed054dab
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-02-18 19:02:27 +0000

    working the about section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,15 +3,29 @@ import { motion } from 'framer-motion';
 
 import './About.scss';
 
+const abouts = [
+  { title: 'Front-end Developer', description: 'I am a great web developer', imgUrl: '' },
+  { title: 'Back-end Developer', description: 'I am a great web developer', imgUrl: '' },
+  { title: 'UI/UX', description: 'I am a great web developer', imgUrl: '' },
+  { title: 'Full-Stack Developer', description: 'I am a great web developer', imgUrl: '' },
+]
+
 const About = () => {
   return (
     <>
       <h2 className='head-text'>
-        I know That
+        I Know that
         <span>Good Design</span>
+        <br />
         means
         <span>Good Business</span>
       </h2>
+
+      <div>
+        { abouts.map((about, index) => (
+          
+        )) }
+      </div>
     </>
   )
 }
