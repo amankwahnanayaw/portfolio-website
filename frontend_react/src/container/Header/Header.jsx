59220e6b40d3578b144ddf7473dd2f4e090d6b0d
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-02-18 18:42:54 +0000

    sunday work

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -44,7 +44,7 @@ const Header = () => {
         transition={{ duration: 0.5, delayChildren: 0.5}}
         className='app__header-img'
       >
-        <img src={images.profile} alt='profile_bg' />
+        <img src={images.profileny} alt='profile_bg' />
         <motion.div
          whileInView={{ scale: [0, 1] }}
          transition={{ duration: 1, ease: 'easeInOut'}}
