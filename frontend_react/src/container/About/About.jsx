52359882ae4079b97ad422a23a4db76f4fd9bcaf
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-02-19 22:42:44 +0000

    adding an HOC wrapper

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,21 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-import { images } from '../../constants'; 
 import './About.scss';
+import { urlFor, client } from '../../client';
 
-const abouts = [
-  { title: 'Web Devloper', description: 'Working on the web is my passion as I love to work on exciting projects.', imgUrl: images.about01 },
-  { title: 'Web Design', description: 'I love working with clients to help them create their dream website.', imgUrl: images.about02 },
-  { title: 'Full-Stack Developer', description: 'I have a strong command over both front-end and back-end technologies, building complete and scalable applicationd', imgUrl: images.about03 },
-  { title: 'UI/UX', description: ' am passionate about creating intuitive user interfaces that provides an exceptional user experience.', imgUrl: images.about04 },
-]
 
 const About = () => {
   const [abouts, setAbouts] = useState([]);
 
   useEffect(() => {
-    const query = '*[_type == "abouts"]'
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query)
+    .then((data) => setAbouts(data))
   }, []);
   
 
@@ -34,7 +31,7 @@ const About = () => {
             className='app__profile-item'
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title}/>
+            <img src={urlFor(about.imgUrl)} alt={about.title}/>
             <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
             <p className='p-text' style={{ marginTop: 20 }}>{about.description}</p>
           </motion.div>
