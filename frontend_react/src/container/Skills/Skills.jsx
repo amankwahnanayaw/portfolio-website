aa24765c083aada49d7c1d0ab2c3f7d47472cc03
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-02-20 22:11:46 +0000

    working on the skills

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -32,7 +32,16 @@ const Skills = () => {
 
       <div className='app__skills-container'>
         <motion.div className='app__skills-list'>
+          {skills.map((skill) => (
+            <motion.div
+            whileInView={{opacity: [0, 1]}}
+            transition={{ duration: 0.5 }}
+            className='app__skills-item app__flex'
+            key={skill.name}
+            >
 
+            </motion.div>
+          ))}
         </motion.div>
       </div>
     </>
