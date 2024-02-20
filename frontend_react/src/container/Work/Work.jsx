e68afdc694bac9ea02cd51b7f1105a2b59d9143f
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-02-20 09:46:37 +0000

    working the working section

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -7,6 +7,27 @@ import { urlFor, client } from '../../client';
 import './Work.scss';
 
 const Work = () => {
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
+
+
+  useEffect(() => {
+    const query = '*[_type == "works"]';
+
+    client.fetch(query)
+      .then((data) => {
+        setWorks(data);
+        setFilterWork(data);
+      })
+  }, [])
+  
+
+  const handleWorkFilter = (item) => {
+
+  }
+
   return (
     <>
       <h2 className='head-text'>My Creative <span>Portfolio</span> </h2>
@@ -16,11 +37,26 @@ const Work = () => {
             <div
               key={index}
               onClick={() => handleWorkFilter(item)}
+              className={`app__work-filter app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
             >
-
+                {item}
             </div>
         ))}
       </div>
+
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className='app__work-portfolio'
+      >
+        {filterWork.map((work, index) => (
+          <div className='app__work-item app__flex' key={index}>
+            <div className='app__work-img app__flex'>
+              <img src={urlFor(work.imgUrl)} alt={work.name} />
+            </div>
+          </div>
+        ))}
+      </motion.div>
     </>
   )
 }
