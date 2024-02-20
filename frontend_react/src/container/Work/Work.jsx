38a7dfabaea17ced2550b2f543503d80c9458738
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-02-20 10:58:23 +0000

    working the working section

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -85,6 +85,10 @@ const Work = () => {
             <div className='app__work-content app__flex'>
               <h4 className='bold-text'>{work.title}</h4>
               <p className='p-text' style={{ marginTop: 10 }}>{work.description}</p>
+
+              <div className='app__work-tag app__flex'>
+                <p className='p-text'>{work.tags[0]}</p>
+              </div>
             </div>
 
           </div>
@@ -94,4 +98,4 @@ const Work = () => {
   )
 }
 
-export default Work
+export default AppWrap(Work, 'work')
