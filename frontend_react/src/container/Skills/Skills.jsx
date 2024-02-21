0dc16d1e3a2a6c9fcb7ce789b5567dbc0a018660
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-02-21 13:22:34 +0000

    styling skills section

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -32,7 +32,7 @@ const Skills = () => {
 
       <div className='app__skills-container'>
         <motion.div className='app__skills-list'>
-          {skills.map((skill) => (
+          {skills?.map((skill) => (
             <motion.div
             whileInView={{opacity: [0, 1]}}
             transition={{ duration: 0.5 }}
@@ -48,29 +48,42 @@ const Skills = () => {
         </motion.div>
 
         <motion.div className='app__skills-exp'>
-            {experience.map((work) =>(
-              <>
-                <motion.div
-                  whileInView={{opacity: [0, 1]}}
-                  transition={{ duration: 0.5 }}
-                  className='app__skills-exp-work'
-                  data-tip
-                  data-for={work.name}
-                  key={work.name}
-                >
-                  <h4 className='bold-text'>{work.name}</h4>
-                  <p className='p-text'>{work.company}</p>
+            {experience?.map((work) =>(
+              <motion.div
+                className='app__skills-exp-item'
+                key={experience.year}
+              >
+                <div className='app__skills-exp-year'>
+                  <p className='bold-text'>{experience.year}</p>
+                </div>
+
+                <motion.div>
+                  {experience.work.map((work) => (
+                    <>
+                    <motion.div
+                      whileInView={{opacity: [0, 1]}}
+                      transition={{ duration: 0.5 }}
+                      className='app__skills-exp-work'
+                      data-tip
+                      data-for={work.name}
+                      key={work.name}
+                    >
+                      <h4 className='bold-text'>{work.name}</h4>
+                      <p className='p-text'>{work.company}</p>
+                    </motion.div>
+    
+                    <ReactTooltip
+                      id={work.name}
+                      effect='solid'
+                      arrowColor='#ff'
+                      className='skills-tooltip'
+                    >
+                      {work.desc}
+                    </ReactTooltip>
+                  </>
+                  ))}
                 </motion.div>
-
-                <ReactTooltip
-                  id={work.name}
-                  effect='solid'
-                  arrowColor='#ff'
-                  className='skills-tooltip'
-                >
-
-                </ReactTooltip>
-              </>
+              </motion.div>
             ))}
 
         </motion.div>
