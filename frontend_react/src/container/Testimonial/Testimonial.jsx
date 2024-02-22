6e1a2e839a17286dc2969470a0a0f55eda3001cc
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-02-22 10:15:12 +0000

    testimonials

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -11,9 +11,55 @@ const Testimonial = () => {
   const [testimonials, setTestimonials] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  useEffect(() => {
+    const query = '*[_type == "testimonials"]';
+    const brandsQuery = '*[_type == "brands"]';
+
+    client.fetch(query)
+      .then((data) => {
+        setTestimonials(data);
+      })
+
+    client.fetch(brandsQuery)
+      .this((data) => {
+        setBrands(data);
+      })
+
+  }, [])
+  
+  const test = testimonials[currentIndex];
+
   return (
-    <div>Testimonial</div>
+    <>
+     {testimonials.length && (
+      <>
+        <div className='app__testimonial-item app__flex'>
+          <img src={urlFor(test.imgurl)} alt='testimonial' />
+          <div className='app__testimonial-content'>
+            <p className='p-text'>{test.feedback}</p>
+            <div>
+              <h4 className='bold-text'>{test.name}</h4>
+              <h5 className='p-text'>{test.company}</h5>
+            </div>
+          </div> 
+        </div>
+
+        <div className='app__testimonials-btns app__flex'>
+          <div className='app__flex' onClick={() => handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}>
+            <HiChevronLeft />
+          </div>
+          <div className='app__flex' onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex  + 1)}>
+            <HiChevronRight />
+          </div>
+        </div>
+      </>
+     )}
+    </>
   )
 }
 
-export default Testimonial
+export default AppWrap(
+  MotionWrap(Testimonial, 'app__testimonial'),
+  'testimonial',
+  "app__primarybg"
+  )
