754bf9fc514745b20f17564a9eb280aad2839add
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-02-24 11:05:23 +0000

    working on footer

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -11,7 +11,10 @@ const Footer = () => {
       <h2 className='head-text'>Take a coffee & chat with me</h2>
 
       <div className='app__footer-cards'>
-
+        <div className='app__footer-card'>
+          <img src={images.email} alt='email' />
+          <a href='mailto:amankwahnanayaw5@gmail.com' className='p-text'>amankwahnanayaw5@gmail.com</a>
+        </div>
       </div>
     </>
   )
