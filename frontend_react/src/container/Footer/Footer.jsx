8e024c5f60954e22ade1bfa3cf23b94d4bc4bad3
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-03-05 21:18:02 +0000

    working the footer

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -52,26 +52,29 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className='app__footer-form app__flex'>
-        <div className='app__flex' >
-          <input className='p-text' type='text' placeholder='Your Name' name='name' value={name} onChange={handleChangeInput}/>
+      {!isFormSubmitted ? 
+        <div className='app__footer-form app__flex'>
+          <div className='app__flex' >
+            <input className='p-text' type='text' placeholder='Your Name' name='name' value={name} onChange={handleChangeInput}/>
+          </div>
+          <div className='app__flex' >
+            <input className='p-text' type='email' placeholder='Your Email' name='email' value={email} onChange={handleChangeInput}/>
+          </div>
+          <div>
+            <textarea 
+              className='p-text'
+              placeholder='Your Message'
+              name='message'
+              value={message}
+              onChange={handleChangeInput}
+              
+            />
+          </div>
+          <button type='button' className='p-text' onClick={handleSubmit}>{loading ? 'Sending' : 'Send Message'}</button>
         </div>
-        <div className='app__flex' >
-          <input className='p-text' type='email' placeholder='Your Email' name='email' value={email} onChange={handleChangeInput}/>
-        </div>
-        <div>
-          <textarea 
-            className='p-text'
-            placeholder='Your Message'
-            name='message'
-            value={message}
-            onChange={handleChangeInput}
-            
-          />
-        </div>
-        <button type='button' className='p-text' onClick={handleSubmit}>{loading ? 'Sending' : 'Send Message'}</button>
-      </div>
-
+        : <div>
+            <h3 className='head-text'>Thank you for getting in touch!</h3>
+        </div> }
     </>
   )
 }
